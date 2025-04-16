01c8cf597c88b6344738cfde7f0ad11e4bb2c2ff
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-16 20:44:17 +0530

    Update views.py

--- src/api/views.py
+++ src/api/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from students.models import Student
 
 # Create your views here.
 
 
 def students_view(request):
-    students = [
-        {"name": "Alice", "age": 20},
-        {"name": "Bob", "age": 22},
-        {"name": "Charlie", "age": 23}
-    ]
-    
+    students = list(Student.objects.values())
+
     return JsonResponse(students, safe=False)
