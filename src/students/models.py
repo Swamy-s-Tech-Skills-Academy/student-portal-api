98ef22feae3b360dee5775a23cc7876d0394efe3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-16 20:40:40 +0530

    Changes

--- src/students/models.py
+++ src/students/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Student(models.Model):
+    student_id = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    batch = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.student_id}. {self.name}"
