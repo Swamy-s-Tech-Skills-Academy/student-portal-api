22536a8d547d0d7ffc0e11dd0ef511de96eef998
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-17 23:14:22 +0530

    Changes

--- src/students/urls.py
+++ src/students/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.students_list, name='students_list'),
+    path('<str:student_id>/', views.student_detail_html_view, name='student_detail_html_view'),
 ]
