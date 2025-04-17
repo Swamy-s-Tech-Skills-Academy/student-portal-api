b6af928c5ff5157bb99e8d0a4a1e817ae10f4e98
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-17 13:32:49 +0530

    Swamy/17apr work (#5)
    * Update home.html
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Update base.html
    
    * Update navbar.html
    
    * Update navbar.html
    
    * Update settings.py
    
    * Changes
    
    * Changes
    
    * Changes

--- src/student_portal_main/settings.py
+++ src/student_portal_main/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',   # Added Django REST framework. Serializers and views will be created in the api app.
+    # Added Django REST framework. Serializers and views will be created in the api app.
+    'rest_framework',
     'students',
     'api',
 ]
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Include the static directory
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
